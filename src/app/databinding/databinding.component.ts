7b8bcedf48e798ed07e1f70526de821d21c83ff5
murugesan33 <60940143+murugesan33@users.noreply.github.com>
2020-06-01 12:22:18 +0530

    dfsdfds
    sfdsfdsf

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+})
+export class DatabindingComponent implements OnInit {
+  username = '';
+  constructor() { }
+
+  ngOnInit() { }
+}
